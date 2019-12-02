420eaa84bcdff7233abe085720a510413f0e1c71
wbingcs <421768544@qq.com>
2019-12-02 14:11:34 +0800

    test

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Hello from "@/components/hello";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Hello from '@/components/hello'
 
-import "./index.less";
+import './index.less'
 
 interface IProps {}
 
 const App: React.SFC<IProps> = ({}) => {
-  var a = "2";
+  var a = '2'
   return (
-    <div className="a">
+    <div className='a'>
       oo0
-      <Hello text="Electron" />
+      <Hello text='Electron' />
     </div>
-  );
-};
+  )
+}
 
 export default () => {
-  ReactDOM.render(<App />, document.querySelector("#root"));
-};
+  ReactDOM.render(<App />, document.querySelector('#root'))
+}
